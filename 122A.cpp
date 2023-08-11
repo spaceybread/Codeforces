45ef7583d102ceddc7b1e0769c92a6b2ceca085e
spaceybread <Spaceybread@gmail.com>
2023-08-11 19:57:43 +0530

    Create 122A.cpp

---
+++ 122A.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<math.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    
+    long long int n;
+    
+    cin >> n;
+    
+    if (n % 4 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 7 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 44 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 77 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 47 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 74 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 444 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 447 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 474 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 744 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 477 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 747 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 774 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    if (n % 777 == 0) {
+        cout << "YES";
+        exit(0);
+    }
+    
+    cout << "NO";
+}
+
+
