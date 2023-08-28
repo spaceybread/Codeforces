b82839ee2046734315e9bff0a49aedfdb0099623
spaceybread <Spaceybread@gmail.com>
2023-08-28 06:35:27 +0530

    Create cTemplateGen.py

---
+++ cTemplateGen.py
@@ -0,0 +1,15 @@
+f = open("cTemplate.cpp", "w")
+
+f.write("#include <iostream>\n")
+f.write("#include <math.h>\n")
+f.write("#include <string>\n")
+f.write("#include <cmath>\n\n")
+
+f.write("using namespace std;\n\n")
+
+f.write("int main() {\n\n")
+
+f.write("}")
+
+f.close()
+
